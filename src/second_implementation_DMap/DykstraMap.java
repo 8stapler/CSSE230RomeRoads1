39566f88a7c9e29d1abb53b78456216c6cfd5589
archerma <archerma@archerma-stg4.rose-hulman.edu>
2019-11-12 19:15:33 -0500

    "Almost completely debugged"

---
+++ src/second_implementation_DMap/DykstraMap.java
@@ -0,0 +1,317 @@
+package second_implementation_DMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+
+public class DykstraMap<E extends Comparable<?super E>> {
+	public static final int approxSites=50;
+	public static final int MAXSHORTESTPATHS=50000;
+	
+	HashMap<String,Site> sites;
+	TreeSet<Site> siteList;
+	private MyInteger pathCount;
+	private int minCost;
+	private ArrayList<Path> paths;
+	
+	private HashMap<String,Road> prohibitedRoads;
+	
+	public DykstraMap() {
+		sites= new HashMap<String, Site>(approxSites);
+		siteList= new TreeSet<Site>();
+		paths = new ArrayList<Path>();
+		pathCount= new MyInteger(0);
+		minCost=-1;
+	}
+	
+	public boolean add(Site toAdd) {
+		boolean added=siteList.add(toAdd);
+		sites.put(toAdd.getName(),toAdd);
+		return added;
+	}
+	public boolean remove(Site toKill) {
+		if(siteList.size()==0)return false;
+		boolean removed = siteList.remove(toKill);
+		sites.remove(toKill.getName());
+		return removed;
+	}
+	public ArrayList<String> listSites(){
+		ArrayList<String> temp = new ArrayList<String>(siteList.size());
+		for(Site site:siteList) {
+			temp.add(site.getName());
+		}
+		
+		return temp;
+	}
+
+	public TreeSet<Site> getSiteList() {
+		return siteList;
+	}
+	
+	public Path shortestPath(Site start, Site end, TreeSet<Site> fullGraph){
+
+		//Set up known arrayList & unknown priorityQueue
+		start.setDistFrom(0);
+		
+		ArrayList<CostCompSite> known = new ArrayList<CostCompSite>();
+		PriorityQueue<CostCompSite> unknown = new PriorityQueue<CostCompSite>();
+		
+		for(Site s: fullGraph) {
+			unknown.add(s.getCostComp());
+		}
+		
+		//Calculate distTo Current's neighbors until current is destination
+		CostCompSite current = start.getCostComp();
+		
+		while(unknown.peek()!=null) {
+			
+			current=unknown.poll();
+			
+			
+			if(current.getMySite().toString().equals(end.toString())) {
+				break;
+			}
+			
+			for(Road r: current.getMySite().getRoads()) {
+				
+				//Never Goes here but it should : WHY?
+				
+				int distFromHere = current.getMySite().getDistFrom() + r.getTimeCost();
+				
+				if(distFromHere < r.getSite().getDistFrom()) {
+					r.getSite().setDistFrom(distFromHere);
+					
+					//Make able to trace back to start (determine path)
+					r.getSite().setToRoad(r);
+					r.getSite().setPrevSite(current.getMySite());
+					
+					//Update priorityQueue
+					unknown.remove(r.getSite().getCostComp());
+					unknown.add(r.getSite().getCostComp());
+				}
+				
+			}
+			
+		}
+		//At this point, we should have the shortest path
+		//Work back from end to get shortest path
+			
+		
+		
+		
+		LinkedList<Road>temp = new LinkedList<Road>();
+		while(current!=null && current.getMySite().getToRoad()!=null) {
+			
+			temp.addFirst(current.getMySite().getToRoad());
+			current=current.getMySite().getPrevSite().getCostComp();
+			
+		}
+		return new Path(temp);
+		
+	}
+	
+	
+	public Path scenestPath(Site start, Site end, int maxCost, ArrayList<Road> bannedRoads) { // Will only check at most the first 100,000 short paths (calculated)
+		
+		return null;
+	}
+	
+
+
+public class CostCompSite implements Comparable{
+
+	private Site mySite;
+	
+	public CostCompSite(Site s) {
+
+		mySite = s;
+		
+	}
+	
+	public int compareTo(Object otherSite) {
+		int compare =mySite.getDistFrom()-(((CostCompSite)otherSite).mySite.getDistFrom());
+		
+		if(compare==0)return 0; 
+			
+			
+		return compare/Math.abs(compare);
+	}
+	public String toString() {
+		return mySite.toString();
+	}
+	
+	public Site getMySite() {
+		return mySite;
+	}
+		
+	}
+	
+public class Site implements Comparable {
+
+		private int xPos;
+		private int yPos;
+		private int hist;
+		private String name;
+		private String desc;
+		private ArrayList<Road> roads= new ArrayList<Road>();
+		private int distFrom;
+		private int sceneFrom;
+		private int histFrom;
+		private CostCompSite costComp;
+		private Site prevSite;
+		private Road toRoad;
+		
+		public Site(int xPosition, int yPosition, int history, String name, String description, ArrayList<Road> roadybois) {
+			
+			xPos=xPosition;
+			yPos=yPosition;
+			hist=history;
+			this.name=name;
+			desc=description;
+			roads=roadybois;
+			distFrom=Integer.MAX_VALUE;
+			sceneFrom=-1;
+			histFrom=-1;
+			
+			costComp=new CostCompSite(this);
+		}
+		
+		public CostCompSite getCostComp() {
+			return costComp;
+			
+		}
+		public void setToRoad(Road a) {
+			toRoad = a;
+		}
+		public Road getToRoad() {
+			return toRoad;
+		}
+		public void setPrevSite(Site a) {
+			prevSite=a;
+		}
+		public Site getPrevSite() {
+			return prevSite;
+		}
+		public ArrayList<Road> getRoads(){
+			return roads;
+		}
+		public void setRoads(ArrayList<Road>r) {
+			roads = r;
+		}
+		public String toString() {
+			return name;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setCostComp(CostCompSite a) {
+			costComp=a;
+		}
+
+		@Override
+		public int compareTo(Object otherSite) {
+			
+			return this.getName().compareTo(((Site)otherSite).getName());
+		}
+		
+		public boolean equals(Site b) {
+			return name.equals(b.getName());
+			
+		}
+
+		public int getHistory() {
+			return hist;
+		}
+		public String getDescription() {
+			return desc;
+		}
+		public int getDistFrom() {
+			return distFrom;
+		}
+		public void setDistFrom(int a) {
+			distFrom=a;
+		}
+		public int getSceneFrom() {
+			return sceneFrom;
+		}
+		public void setSceneFrom(int a) {
+			sceneFrom=a;
+		}
+		public int getHistFrom() {
+			return histFrom;
+		}
+		public void setHistFrom(int a) {
+			histFrom=a;
+		}
+}
+
+public class Road{
+	private Site destination;
+	private int beauty;
+	private int timeCost;
+	private String name;
+	
+	public Road(String n, Site dest, int beaut, int tic) {
+		name=n;
+		destination = dest;
+		beauty = beaut;
+		timeCost = tic;
+	}
+	
+	public Site getSite() {
+		return  destination;
+	}
+	
+	public int getBeauty() {
+		return beauty;
+	}
+	
+	public int getTimeCost() {
+		return timeCost;
+	}
+
+	public String getName() {
+
+		return name;
+	}
+	
+	
+}
+
+public class Path extends ArrayList<Road>{
+
+		private int length;
+		
+		public Path(LinkedList<Road> roads) {
+			super(roads);
+		}
+		
+		public String toString() {
+			ArrayList<String> result = new ArrayList<String>();
+			for(Road r:this) {
+				result.add(r.getName());
+			}
+			return result.toString();
+		}
+	}
+
+public class MyInteger{
+	
+	private int value;
+	
+	public MyInteger(int a) {
+		setValue(a);
+	}
+	
+	public void setValue(int a) {
+		value = a;
+	}
+	public int getValue() {
+		return value;
+	}
+}
+
+}
---
+++ src/second_implementation_DMap/DykstraTest.java
@@ -0,0 +1,117 @@
+package second_implementation_DMap;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import second_implementation_DMap.DykstraMap.Road;
+import second_implementation_DMap.DykstraMap.Site;
+import second_implementation_DMap.DykstraMap.CostCompSite;
+import second_implementation_DMap.DykstraMap.Path;
+
+public class DykstraTest {
+	DykstraMap m = new DykstraMap();
+	
+	Site site1 = m.new Site(0,0,0,"1", "desc1", null);
+	Site site2 = m.new Site(0,0,0,"2", "desc2", null);
+	Site site3 = m.new Site(0,0,0,"3", "desc3", null);
+	Site site4 = m.new Site(0,0,0,"4", "desc4", null);
+	Site site5 = m.new Site(0,0,0,"5", "desc5", null);
+	Site site6 = m.new Site(0,0,0,"6", "desc6", null);
+	Site site7 = m.new Site(0,0,0,"7", "desc7", null);
+	
+	Road r1_2 = m.new Road("1-2", site2, 10, 3);
+	Road r2_1 = m.new Road("2-1", site1, 10, 3);
+	Road r1_3 = m.new Road("1-3", site3, 5, 7);
+	Road r3_1 = m.new Road("3-1", site1, 5, 7);
+	Road r1_4 = m.new Road("1-4", site4, 7, 4);
+	Road r4_1 = m.new Road("4-1", site1, 7, 4);
+	Road r2_3 = m.new Road("2-3", site3, 3, 3);
+	Road r3_2 = m.new Road("3-2", site2, 3, 3);
+	Road r3_6 = m.new Road("3-6", site6, 3, 3);
+	Road r6_3 = m.new Road("6-3", site3, 3, 3);
+	Road r4_6 = m.new Road("4-6", site6, 4, 4);
+	Road r6_4 = m.new Road("6-4", site4, 4, 4);
+	Road r2_5 = m.new Road("2-5", site5, 5, 5);
+	Road r5_2 = m.new Road("5-2", site2, 5, 5);
+	Road r5_7 = m.new Road("5-7", site7, 2, 2);
+	Road r7_5 = m.new Road("7-5", site5, 2, 2);
+	Road r6_7 = m.new Road("6-7", site7, 3, 3);
+	Road r7_6 = m.new Road("7-6", site6, 3, 3);
+	
+	public void testDykstraConstruct() {//tests constructors + makes the graph we'll use for the rest of the tests
+	
+	ArrayList<Road> roads = new ArrayList<Road>();
+	
+	roads.add(r1_2);
+	roads.add(r1_3);
+	roads.add(r1_4);
+	
+	site1.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r2_1);
+	roads.add(r2_3);
+	roads.add(r2_5);
+	
+	site2.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r3_1);
+	roads.add(r3_2);
+	roads.add(r3_6);
+	
+	site3.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r4_1);
+	roads.add(r4_6);
+	
+	site4.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r5_2);
+	roads.add(r5_7);
+	
+	site5.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r6_3);
+	roads.add(r6_4);
+	roads.add(r6_7);
+	
+	site6.setRoads(roads);
+	roads.clear();
+	
+	roads.add(r7_5);
+	roads.add(r7_6);
+	
+	site7.setRoads(roads);
+	roads.clear();
+	
+	m.add(site1);
+	m.add(site2);
+	m.add(site3);
+	m.add(site4);
+	m.add(site5);
+	m.add(site6);
+	m.add(site7);
+}
+	@Test
+	public void basicTest() {
+		testDykstraConstruct();
+		//from 1 to 2
+		LinkedList<Road> spbase = new LinkedList<Road>();
+		spbase.add(r1_2);
+		Path sp = m.new Path(spbase);
+		assertEquals(sp,(m.shortestPath(site1, site2, m.getSiteList())));
+		
+		spbase.add(r2_3);
+		sp = m.new Path(spbase);
+		assertEquals(sp,(m.shortestPath(site1, site3,m.getSiteList())));
+	}
+}
